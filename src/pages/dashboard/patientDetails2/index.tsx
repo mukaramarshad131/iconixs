2c4c857991d60af73c4e589cebb7f5bca55413c7
maria-maqbool <mariamaqbool35@gmail.com>
2024-08-24 16:14:38 +0500

    restructuring

---
+++ src/pages/dashboard/patientDetails2/index.tsx
@@ -0,0 +1,12 @@
+import GeneralTab from './general-tab';
+
+function PatientDetails() {
+  // const theme = useThemeToken();
+  return (
+    <div className="p-2">
+      <GeneralTab />
+    </div>
+  );
+}
+
+export default PatientDetails;
