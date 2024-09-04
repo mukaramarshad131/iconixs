e4ae876e2ae9118e5f485b0088911eb29ebbc10b
shahroozdev <shahroozdeveloper@gmail.com>
2024-09-04 23:09:53 +0500

    some fixing

--- src/components/packages-card/index.tsx
+++ src/components/packages-card/index.tsx
@@ -33,13 +33,13 @@ const businessPackages = [
 function PackagesCard() {
   // const navigate = useNavigate();
   const { setUserPlan } = useUserActions();
-  const { firstName, lastName, zip, state } = useUserInfo();
+  const { email, firstName, lastName, zip, state } = useUserInfo();
 
   const handleCart = (planId: string) => {
     setUserPlan({ planId });
 
     window.open(
-      `https://iconix-test.chargebee.com/hosted_pages/checkout?subscription_items[item_price_id][0]=${planId}-USD-Monthly&subscription_items[quantity][0]=1&customer[first_name]=Jose&customer[last_name]=Diaz&&customer[email]=diaz@gmail.com&billing_address[first_name]=${firstName}&billing_address[last_name]=${lastName}&billing_address[line1]=AddressLine1&billing_address[line2]=AddressLine2&billing_address[city]=Maxico&billing_address[zip]=${zip}&billing_address[state_code]=${state}&billing_address[country]=US`,
+      `https://iconix-test.chargebee.com/hosted_pages/checkout?subscription_items[item_price_id][0]=${planId}-USD-Monthly&subscription_items[quantity][0]=1&customer[first_name]=${firstName}&customer[last_name]=${lastName}&&customer[email]=${email}&billing_address[first_name]=${firstName}&billing_address[last_name]=${lastName}&billing_address[line1]=AddressLine1&billing_address[line2]=AddressLine2&billing_address[city]=Maxico&billing_address[zip]=${zip}&billing_address[state_code]=${state}&billing_address[country]=US`,
       '_blank',
     );
     // navigate('/dashboard/checkout');
