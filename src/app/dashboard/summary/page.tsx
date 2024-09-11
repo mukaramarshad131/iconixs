002e69027a48f24e9187add1a8faed2b8c290066
maria-maqbool <mariamaqbool35@gmail.com>
2024-09-11 19:31:30 +0500

    update summary page

--- src/app/dashboard/summary/page.tsx
+++ src/app/dashboard/summary/page.tsx
@@ -1,3 +1,4 @@
+import IntakeListing from '@/components/pages/dashboard/intake-Listing';
 import Medication from '@/components/pages/dashboard/medication';
 import NewSummary from '@/components/pages/dashboard/new-summary';
 import { Metadata } from 'next';
@@ -9,6 +10,7 @@ export const dynamic = 'force-dynamic';
 const Summary = () => {
   return (
     <div>
+      <IntakeListing />
       <NewSummary />
       <Medication />
     </div>
