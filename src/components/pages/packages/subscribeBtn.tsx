d105feb78a842a31445af04edfcc080bb61b3441
maria-maqbool <mariamaqbool35@gmail.com>
2024-09-12 13:21:34 +0500

    modify changes

--- src/components/pages/packages/subscribeBtn.tsx
+++ src/components/pages/packages/subscribeBtn.tsx
@@ -12,7 +12,6 @@ const SubscribeBtn = ({data}: {data:any}) => {
   const { setUserPlan, setUserPermissions } = useUserActions();
   const permissions = useUserPermissions();
   const { email, first_name, last_name, location } = useUserInfo();
-  console.log('location:', location);
   const router = useRouter();
   const handleCart = (planId: string) => {
     setUserPlan(planId);
@@ -22,7 +21,7 @@ const SubscribeBtn = ({data}: {data:any}) => {
     // https://iconix-test.chargebee.com/hosted_pages/checkout?subscription_items[item_price_id][0]=ico-trt-kyzatrex-oral-28-USD-Every-28-days&utm_source=cb-app-copy
     router.replace(
       process.env.CHARGEBEE_URL +
-        `?subscription_items[item_price_id][0]=${planId}-USD-Every-28-days&subscription_items[quantity][0]=1&customer[first_name]=${first_name}&customer[last_name]=${last_name}&&customer[email]=${email}&billing_address[first_name]=${first_name}&billing_address[last_name]=${last_name}&billing_address[line1]=AddressLine1&billing_address[line2]=AddressLine2&billing_address[city]=Maxico&billing_address[zip]=${location.zip}&billing_address[state_code]=${location.state}&billing_address[country]=US`
+        `?subscription_items[item_price_id][0]=${planId}-USD-Every-28-days&subscription_items[quantity][0]=1&customer[first_name]=${first_name}&customer[last_name]=${last_name}&&customer[email]=${email}&billing_address[first_name]=${first_name}&billing_address[last_name]=${last_name}&billing_address[line1]=${location.line1}&billing_address[city]=${location.city}&billing_address[zip]=${location.zip}&billing_address[state_code]=${location.state}&billing_address[country]=US`
     );
   };
 
