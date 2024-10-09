0f3975f35708d04a4b3877b52cd404b73a1f8cbb
maria-maqbool <mariamaqbool35@gmail.com>
2024-10-09 21:00:01 +0500

    add prod ids

--- src/components/pages/intakeForm/index.tsx
+++ src/components/pages/intakeForm/index.tsx
@@ -54,11 +54,12 @@ export default function ItakeForm() {
 
   const { data: intakeFormData } = useQuery(INTAKE_FORM_QUERY, {
     variables: {
-      custom_module_form_id: "1524146",
+      custom_module_form_id: "2174074",
       user_id: user.id,
       filler_id: user.id,
     },
   });
+  console.log('intakeFormData:', intakeFormData);
   const result = extractQuestionsAndAnswers(
     intakeFormData?.formAnswerGroups[0]?.form_answers[2]?.displayed_answer
   );
@@ -68,46 +69,46 @@ export default function ItakeForm() {
     console.log('upload_social_security: ', upload_social_security);
     const intakeFormPayload = {
       input: {
-        custom_module_form_id: "1524146", // Form id for staging
+        custom_module_form_id: "2174074", // Form id for staging
         form_answers: [
           {
-            custom_module_id: "14669225",
+            custom_module_id: "29460116",
             label: "Patient Info",
           },
           {
-            custom_module_id: "14669226",
+            custom_module_id: "29460117",
             label: "Driver License Number (DL)",
             user_id: user.id,
             answer: license_number,
           },
           {
-            custom_module_id: "14669227",
+            custom_module_id: "29460118",
             label: "Social Security Number (SSN)",
             answer: security_number,
           },
+          // {
+          //   custom_module_id: "14669228",
+          //   label: "Upload Social Driving Liscense",
+          //   mod_type: "textarea",
+          //   answer: upload_driving_liscense,
+          // },
+          // {
+          //   custom_module_id: "14669229",
+          //   label: "Upload Social Security Number",
+          //   mod_type: "textarea",
+          //   answer: upload_social_security,
+          // },
           {
-            custom_module_id: "14669228",
-            label: "Upload Social Driving Liscense",
-            mod_type: "textarea",
-            answer: upload_driving_liscense,
-          },
-          {
-            custom_module_id: "14669229",
-            label: "Upload Social Security Number",
-            mod_type: "textarea",
-            answer: upload_social_security,
-          },
-          {
-            custom_module_id: "13579507",
+            custom_module_id: "28756629",
             label: "Intake",
           },
           {
-            custom_module_id: "13579508",
+            custom_module_id: "28756630",
             label: "Hormone Type",
             answer: "TRT", // HTML format for the intake
           },
           {
-            custom_module_id: "13579509",
+            custom_module_id: "28756631",
             label: "Patient Intake",
             user_id: user.id,
             answer: `<p>${Object.values(questionValues)
@@ -125,371 +126,184 @@ export default function ItakeForm() {
               } ${user.location?.zip}</p>`, // HTML format for the intake
           },
           {
-            custom_module_id: "13579510",
+            custom_module_id: "28756632",
             label: "Photo Upload",
             answer: "photo",
             user_id: user.id, // HTML format for the intake
           },
           {
-            custom_module_id: "13579511",
+            custom_module_id: "28756633",
             label: "Labs will be ordered through",
             answer: "At Home Labs ( at_home_labs_vital )",
             user_id: user.id,
           },
           {
-            custom_module_id: "13085533",
+            custom_module_id: "28565764",
             label: "Sync/Async Visit",
             mod_type: "dropdown",
             answer: "Async ( async_visit )",
             user_id: user.id,
           },
           {
-            custom_module_id: "13579512",
+            custom_module_id: "28756634",
             label: "Charting",
             answer: "Charting",
             user_id: user.id,
           },
           {
-            custom_module_id: "13085531",
+            custom_module_id: "28565762",
             label: "Note Type",
             answer: "Test Note Type",
             user_id: user.id,
           },
           {
-            custom_module_id: "13085532",
+            custom_module_id: "29460121",
             label: "Visit Type",
             answer: "Initial Visit ( visit_type_1 )",
             user_id: user.id,
           },
           {
-            custom_module_id: "13085561",
+            custom_module_id: "28565792",
             label: "Patient Consent (Sync)",
             answer: "Patient Consent (Sync)",
             user_id: user.id,
           },
           {
-            custom_module_id: "13085559",
+            custom_module_id: "28565790",
             label: "Patient Consent (Async)",
             answer: "Patient Consent (Async)",
             user_id: user.id,
           },
           {
-            custom_module_id: "13085543",
+            custom_module_id: "28565774",
             label: "Visit Modality",
             answer: "Test Visit Modality",
             user_id: user.id,
           },
           {
-            custom_module_id: "13085567",
+            custom_module_id: "28565798",
             label: "What state is the patient located in at time of visit?",
             answer: "Test",
             user_id: user.id,
           },
           {
-            custom_module_id: "13085566",
+            custom_module_id: "28565797",
             label: null,
             answer: "Test",
             user_id: user.id,
           },
           {
-            custom_module_id: "13085569",
+            custom_module_id: "28565811",
             label: null,
             answer: "Test",
             user_id: user.id,
           },
           {
-            custom_module_id: "13085535",
+            custom_module_id: "28565766",
             label: "Name of Patient",
             answer: "",
             user_id: user.id,
           },
           {
-            custom_module_id: "13085537",
+            custom_module_id: "28565768",
             label: "Date of Birth",
             answer: "",
             user_id: user.id,
           },
           {
-            custom_module_id: "13085538",
+            custom_module_id: "28565769",
             label: "Gender",
             answer: "Gender",
             user_id: user.id,
           },
           {
-            custom_module_id: "13085562",
+            custom_module_id: "28565793",
             label: "Physical Location of Patient ",
             answer: "",
             user_id: user.id,
           },
           {
-            custom_module_id: "13085571",
+            custom_module_id: "28565802",
             label: "If Other, please explain:",
             answer: "",
             user_id: user.id,
           },
           {
-            custom_module_id: "13085586",
+            custom_module_id: "28565817",
             label: "BMI",
             answer: "",
             user_id: user.id,
           },
           {
-            custom_module_id: "13085539",
+            custom_module_id: "28565770",
             label: "Any known allergies? (Medication, Environmental, or Food)",
             mod_type: "radio",
             answer: "",
             user_id: user.id,
           },
           {
-            custom_module_id: "13085572",
+            custom_module_id: "28565803",
             label: "Allergy",
             answer: "",
             user_id: user.id,
           },
           {
-            custom_module_id: "13085540",
+            custom_module_id: "28565771",
             label:
               "Is patient currently taking any medications (prescription or over-the-counter)",
             answer: "",
             user_id: user.id,
           },
           {
-            custom_module_id: "13085573",
+            custom_module_id: "28565804",
             label: "List any medications you are taking",
             answer: "",
             user_id: user.id,
           },
           {
-            custom_module_id: "13085563",
+            custom_module_id: "28565794",
             label: "Chief Complaint",
             answer: "",
             user_id: user.id,
           },
           {
-            custom_module_id: "13085555",
-            answer: "",
-            user_id: user.id,
-          },
-          {
-            custom_module_id: "13085556",
-            answer: "",
-            user_id: user.id,
-          },
-          {
-            custom_module_id: "13085574",
-            answer: "",
-            user_id: user.id,
-          },
-          {
-            custom_module_id: "13085560",
-            answer: "",
-            user_id: user.id,
-          },
-          {
-            custom_module_id: "13085565",
-            answer: "",
-            user_id: user.id,
-          },
-          {
-            custom_module_id: "13085553",
-            answer: "",
-            user_id: user.id,
-          },
-          {
-            custom_module_id: "13085575",
-            answer: "",
-            user_id: user.id,
-          },
-          {
-            custom_module_id: "13085547",
-            answer: "",
-            user_id: user.id,
-          },
-          {
-            custom_module_id: "13085554",
-            answer: "",
-            user_id: user.id,
-          },
-          {
-            custom_module_id: "13085576",
-            answer: "",
-            user_id: user.id,
-          },
-          {
-            custom_module_id: "13085568",
-            answer: "",
-            user_id: user.id,
-          },
-          {
-            custom_module_id: "13085548",
-            answer: "",
-            user_id: user.id,
-          },
-          {
-            custom_module_id: "13085577",
-            answer: "",
-            user_id: user.id,
-          },
-          {
-            custom_module_id: "13085578",
-            answer: "",
-            user_id: user.id,
-          },
-          {
-            custom_module_id: "13085557",
-            answer: "",
-            user_id: user.id,
-          },
-          {
-            custom_module_id: "13085558",
-            answer: "",
-            user_id: user.id,
-          },
-          {
-            custom_module_id: "13085564",
-            answer: "",
-            user_id: user.id,
-          },
-          {
-            custom_module_id: "13085549",
-            answer: "",
-            user_id: user.id,
-          },
-          {
-            custom_module_id: "13085579",
-            answer: "",
-            user_id: user.id,
-          },
-          {
-            custom_module_id: "13085580",
-            answer: "",
-            user_id: user.id,
-          },
-          {
-            custom_module_id: "13085581",
-          },
-          {
-            custom_module_id: "13085545",
-            answer: "",
-            user_id: user.id,
-          },
-          {
-            custom_module_id: "13886268",
-            answer: "",
-            user_id: user.id,
-          },
-          {
-            custom_module_id: "13886261",
-            answer: "",
-            user_id: user.id,
-          },
-          {
-            custom_module_id: "13886262",
-            answer: "",
-            user_id: user.id,
-          },
-          {
-            custom_module_id: "13886263",
-            answer: "",
-            user_id: user.id,
-          },
-          {
-            custom_module_id: "13085584",
-            answer: "",
-            user_id: user.id,
-          },
-          {
-            custom_module_id: "13886264",
-            answer: "",
-            user_id: user.id,
-          },
-          {
-            custom_module_id: "13886265",
-            answer: "",
-            user_id: user.id,
-          },
-          {
-            custom_module_id: "13886266",
-            answer: "",
-            user_id: user.id,
-          },
-          {
-            custom_module_id: "13886267",
-            answer: "",
-            user_id: user.id,
-          },
-          {
-            custom_module_id: "13085570",
-            answer: "",
-            user_id: user.id,
-          },
-          {
-            custom_module_id: "13085541",
-            answer: "",
-            user_id: user.id,
-          },
-          {
-            custom_module_id: "13085550",
-            answer: "",
-            user_id: user.id,
-          },
-          {
-            custom_module_id: "13085551",
-            answer: "",
-            user_id: user.id,
-          },
-          {
-            custom_module_id: "13085552",
-            answer: "",
-            user_id: user.id,
-          },
-          {
-            custom_module_id: "13085530",
-            answer: "",
-            user_id: user.id,
-          },
-          {
-            custom_module_id: "13085542",
-            answer: "",
-            user_id: user.id,
-          },
-          {
-            custom_module_id: "13085544",
+            custom_module_id: "28565775",
             label: "CPT Codes",
             answer: "",
             user_id: user.id,
           },
           {
-            custom_module_id: "13085546",
+            custom_module_id: "28565777",
             label: "Visit Status",
             answer: "",
             user_id: user.id,
           },
           {
-            custom_module_id: "13085585",
+            custom_module_id: "29576651",
             label:
               "PF - Patient Education & Patient Plan (Patient Facing) - Not Prescribed DQ",
             answer: "",
             user_id: user.id,
           },
           {
-            custom_module_id: "13085534",
+            custom_module_id: "28565765",
             label: "DQ/No-Show/ReScheduled/Cancellation Status",
             answer: "",
             user_id: user.id,
           },
           {
-            custom_module_id: "13085536",
+            custom_module_id: "28565767",
             label: "Additional No-Show/Cancellation/Declined Care Information:",
             answer: "",
             user_id: user.id,
           },
         ],
-        name: "SP - Hormone SOAP Intake",
+        name: "Hormone SOAP Intake",
         finished: true,
         user_id: user.id, // Patiend ID from CreatePatient mutation response
       },
     };
+
 
     // await intakeFormFunction({ variables: { ...intakeFormPayload } });
     setUserIntakeDoc({ upload_driving_liscense, upload_social_security });
