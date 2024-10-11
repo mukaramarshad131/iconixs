4bcc49312ccb184bf8bfd4b1c03ee1795ad615b1
maria-maqbool <mariamaqbool35@gmail.com>
2024-10-11 20:56:08 +0500

    remove payload

--- src/app/api/webhook/route.ts
+++ src/app/api/webhook/route.ts
@@ -1,5 +1,5 @@
 import { NextRequest, NextResponse } from "next/server";
-import { INTAKE_FORM } from "@/graphql/query";
+// import { INTAKE_FORM } from "@/graphql/query";
 import { ApolloClient, InMemoryCache } from "@apollo/client";
 
   const client = new ApolloClient({
@@ -14,45 +14,9 @@ export async function POST(req: NextRequest) {
   const json = await req.json(); // Parse the request body
   // Log the received data for debugging
   console.log('Received Healthie OpenLoop webhook:', json);
-  const intakeFormPayload = {
-    input: {
-      custom_module_form_id: "1524146", // Form id for staging
-      form_answers: [
-        {
-          custom_module_id: "14669225",
-          label: "Patient Info",
-        },
-        {
-          custom_module_id: "13579507",
-          label: "Intake",
-        },
-        {
-          custom_module_id: "13579508",
-          label: "Hormone Type",
-          answer: "TRT", // HTML format for the intake
-        },
-        {
-          custom_module_id: "13579509",
-          label: "Patient Intake",
-          user_id: 1746532,
-          answer: JSON.stringify(json), // HTML format for the intake
-        },
-
-      ],
-      name: "SP - Hormone SOAP Intake",
-      finished: true,
-      user_id: 1746532, // Patiend ID from CreatePatient mutation response
-    },
-  }
-
   try {
-
-    const { data } = await client.query({
-     query: INTAKE_FORM,
-     variables: { ...intakeFormPayload  }, // Pass the email as a variable
-   });
   
-    return NextResponse.json({ message: 'Login successful', user: data}, { status: 200 });
+    return NextResponse.json({ message: 'Login successful', user: json}, { status: 200 });
 
   } catch (error: any) {
     // Improved error logging
