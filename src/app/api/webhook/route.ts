17c90b38ccd79583111291ae16bf6cac8a8bee67
maria-maqbool <mariamaqbool35@gmail.com>
2024-10-12 13:58:56 +0500

    add dummy table

--- src/app/api/webhook/route.ts
+++ src/app/api/webhook/route.ts
@@ -1,7 +1,7 @@
 import { NextRequest, NextResponse } from "next/server";
 // import { INTAKE_FORM } from "@/graphql/query";
+import prisma from "@/lib/prisma";
 import { ApolloClient, InMemoryCache } from "@apollo/client";
-import { CREATE_PATIENT } from "@/graphql/query";
 
   const client = new ApolloClient({
      uri: process.env.OPEN_LOOP_URL,
@@ -14,22 +14,15 @@ import { CREATE_PATIENT } from "@/graphql/query";
 
 export async function POST(req: NextRequest) {
   const json = await req.json(); // Parse the request body
-  const { data } = await client.mutate({
-    mutation: CREATE_PATIENT,
-    variables: { input: {
-      first_name: "Muhammad",
-      last_name: "Ismail",
-      email: "ism@gmail.com",
-      skipped_email: false,
-      dont_send_welcome: true,
-      dietitian_id: process.env.DIETITIAN_ID,
-      phone_number: "5475899",
-    }},
-  });
   // Log the received data for debugging
   console.log('Received Healthie OpenLoop webhook:', json);
   try {
-  
+    await prisma.comment.create({
+      data: {
+        title: "MSK",
+        content: JSON.stringify(json)
+      },
+  })
     return NextResponse.json({ message: 'Login successful', user: json}, { status: 200 });
 
   } catch (error: any) {
@@ -37,4 +30,12 @@ export async function POST(req: NextRequest) {
     console.error("Error in login process:", error.message);
     return NextResponse.json({ error: `Server Error - ${error.message}` }, { status: 500 });
   }
+}
+
+export async function GET() {
+  const ism = await prisma.comment.findMany();
+  const data = { message: "Hello from API Route!" };
+  return new Response(JSON.stringify(ism), {
+    headers: { 'Content-Type': 'application/json' },
+  });
 }
